49726681f7c54fe6b288b65d183f6e192c1b0aee
ark-1309 <113790037+ark-1309@users.noreply.github.com>
2022-10-30 22:45:08 +0300

    changes

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 g++ ./src/sort.cpp -c
-ar rvs ./src/sort.a ./src/sort.o
+ar rvs ./src/libsort.a sort.o
 
 g++ ./src/main.cpp -c
-g++ ./src/main.o ./src/sort.a -o result
+g++ main.o ./src/libsort.a -o result
 
-g++ ./test/test.cpp -c
-g++ ./test/test.o ./test/sort.a -o ./test/test
 
 
